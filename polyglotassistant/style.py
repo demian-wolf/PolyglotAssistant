ac78d0a20de937ca321375caa174be353ff8749a
demian-wolf <demianwolfssd@gmail.com>
2020-12-07 20:09:40 +0200

    updated style

--- polyglotassistant/style.py
+++ polyglotassistant/style.py
@@ -6,25 +6,30 @@ class AppStyles(object):
     def __init__(self, master):
         self.master = master
 
-        self.options = {"*Menu.activeBackground": "#3DAEE9",
-                        "*Menu.background": "#EFF0F1",
+        NORMAL_BG = "#EFF0F1"
+        ACTIVE_BG = "#3DAEE9"
+        ACTIVE_FG = "black"
+
+        self.options = {"*Menu.activeBackground": ACTIVE_BG,
+                        "*Menu.background": NORMAL_BG,
                         "*Menu.activeBorderWidth": 0,
                         "*Menu.borderWidth": 1,
                         "*Menu.tearOff": False,
                         "*Menu.relief": "flat"}
 
-        self.ttk_styles = [("TButton", {"padding": 6,
-                                        "relief": "flat",
-                                        "background": "blue"})]
+        self.ttk_theme = "alt"
+        self.ttk_styles = [] # [("TButton", {"background": NORMAL_BG})]
         self.ttk_styles_maps = [('TButton',
-                                {"background": [('active', '#3DAEE9')],
-                                 "foreground": [('active', 'black')]})]
+                                {"background": [("active", ACTIVE_BG)],
+                                 "foreground": [("active", ACTIVE_FG)]})]
 
     def apply(self):
         for option, value in self.options.items():
             self.master.option_add(option, value)
 
         self.master.style = ttk.Style(self.master)
+
+        self.master.style.theme_use(self.ttk_theme)
 
         for name, style in self.ttk_styles:
             self.master.style.configure(name, **style)
